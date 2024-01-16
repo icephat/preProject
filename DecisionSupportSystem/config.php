4ce007832efd63c6b03f3b33e5e7bcbb5c8b76c7
Panuwat Janjinda <panuwat.janj@ku.th>
2024-01-16 19:28:33 +0700

    Update config.php

--- DecisionSupportSystem/config.php
+++ DecisionSupportSystem/config.php
@@ -1,14 +1,39 @@
 <?php
 
 session_start();
+
+
+include_once 'function/teacherFunction.php';
+//require_once 'function/studentFunction.php';
+
 $_SESSION["system"] = "DecisionSupport";
-$_SESSION["access-user"] = "b6320500611";
+$_SESSION["access-user"] = "fengtps";
+$username = $_SESSION["access-user"];
 
 
 
 
-header('Location: ' . './student/home.php');
-exit();
+
+if (!is_null(getTeacherByUsernameTeacher($username))) {
+    
+
+    $teacher = getTeacherByUsernameTeacher($username);
+    if ($teacher["roleNameEng"] == "teacher") {
+        header('Location: ' . './teacher/home.php');
+        exit();
+    }
+
+} else {
+    echo "เข้า";
+    header('Location: ' . './student/home.php');
+    exit();
+}
+
+
+
+
+
+
 
 
 
