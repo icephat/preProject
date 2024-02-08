79994ac00f01764b219835cb8c267b790db76ec5
icephat <78529872+icephat@users.noreply.github.com>
2024-02-08 12:12:45 +0700

    js

---
+++ DecisionSupportSystem/js/demo/datatables-demoModal.js
@@ -0,0 +1,22 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+  
+    $('#dataTable').DataTable();
+    $('#dataTable2').DataTable();
+    $('#dataTable3').DataTable();
+    $('#dataTable4').DataTable();
+    $('#dataTable5').DataTable();
+    $('#dataTable6').DataTable();
+    $('#dataTable7').DataTable();
+    $('#dataTable8').DataTable();
+    $('#dataTable9').DataTable();
+    $('#dataTable10').DataTable();
+    $('#dataTable11').DataTable();
+    $('#dataTable12').DataTable();
+    $('#dataTable13').DataTable();
+    $('#dataTable14').DataTable();
+    $('#dataTable15').DataTable();
+    $('#dataTable16').DataTable();
+  
+  });
+  
