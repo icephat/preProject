ca85f6bae0341c3343d8b88e85fc770e60a7f263
Panuwat Janjinda <panuwat.janj@ku.th>
2024-03-14 22:24:46 +0700

    Update nisitFacultySearch.php

--- DecisionSupportSystem/teacher/nisitFacultySearch.php
+++ DecisionSupportSystem/teacher/nisitFacultySearch.php
@@ -66,7 +66,7 @@ $deptStudents = getStudentByStudentIdANDStudentName($_POST["search"]);
             <!-- Main Content -->
             <div id="content">
 
-                <?php include('../layout/head/nisit_dep.php'); ?>
+                <?php include('../layout/teacher/home.php'); ?>
                 <hr>
                 <div class="row">
                     <div class="col-12 mx-auto">
